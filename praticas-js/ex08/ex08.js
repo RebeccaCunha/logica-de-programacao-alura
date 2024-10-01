49a26792bfea48cd5a7f8e81ecd788f507561ced
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-10-01 11:03:49 -0300

    ex08

---
+++ praticas-js/ex08/ex08.js
@@ -0,0 +1,51 @@
+/*Criar uma função que exibe "Olá, mundo!" no console.
+function exibirTextoNaTela () {
+    console.log('Olá, mundo!');
+}
+
+exibirTextoNaTela(); 
+*/
+
+/*Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function cumprimentar(nome) {
+    console.log (`Olá, ${nome}!`)
+}
+cumprimentar('Rebecca');
+*/
+
+/*Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function multiplicar(numero) {
+    return numero * 2;
+}
+
+let resultado = multiplicar(5)
+console.log(resultado);
+*/
+
+/*Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function calcularmedia(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+let media = calcularmedia(5, 10, 15);
+console.log(media);
+*/
+
+/*Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maiorNumero(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+let maior = maiorNumero(7,10);
+console.log(maior);
+*/
+
+//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo.
+function multiplicarPorSiMesmo(numero) {
+    return numero * numero
+}
+let resultado = multiplicarPorSiMesmo(5);
+console.log(resultado);
